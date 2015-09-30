495437f3907f8e017a728f3b2c6decb00de7c617
dadiletta <dan.adiletta@gmail.com>
2015-09-29 20:26:09 -0400

    Fleshed out methods

--- src/ideone/Ideone.java
+++ src/ideone/Ideone.java
@@ -1,6 +1,7 @@
 package ideone; //do not include on Ideone.com
 
 import java.util.Scanner; //allows me to take in input
+import java.util.Random;
 
 /**
  * Console League of Legends
@@ -8,8 +9,8 @@ import java.util.Scanner; //allows me to take in input
  * @author adilettad
  */
 public class Ideone {
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+        {
             //REQUIRED: Create at least one instance of a custom class
             Champion player = new Champion();
             Champion npc = new Champion(true);
@@ -18,17 +19,54 @@ public class Ideone {
             //REQUIRED: Some type of game loop.. this one repeats...
             //...so long as both players' health is above 0
             while((player.getHealth() > 0) && (npc.getHealth() > 0)){
-                // attack(playerTurn, player, npc)
-                // whoseTurn(playerTurn)
+                attack(player1, player, npc);
+                player1 = whoseTurn(player1, player, npc);
             }
-            //announce winner... this only runs when the loop is broken b/c...
-            //...a player was defeated
+            announceWinner(player1);
 	}
+
+        /**
+         * Attacks are calculated and damage is registered.
+         * @param player1
+         * @param player
+         * @param npc 
+         */
+        public static void attack(boolean player1, Champion player, Champion npc)
+        {
+            System.out.println("Attack!");
+        }
+        
+        /**
+         * Next player's attack turn is determined
+         * @param player1
+         * @param player
+         * @param npc
+         * @return
+         */
+        public static boolean whoseTurn(boolean player1, Champion player, Champion npc)
+        {
+            System.out.println("Who's turn is it?");
+            return !player1;
+        }
+        
+        /**
+         * Winner is announced depending on the boolean's status
+         * @param player1 
+         */
+        public static void announceWinner(boolean player1)
+        {
+            if (player1)
+            {
+                System.out.println("You won, congratulations");
+            }else{
+                System.out.println("You lost. Try harder next time.");
+            }
+        }
 } //closes the Ideone class
 
 /**
  * REQUIRED: A custom class with all private attributes. One of those...
- * attributes must be a second custom class. 
+ * ..attributes must be a second custom class. 
  * @author adilettad
  */
 class Champion 
@@ -43,24 +81,28 @@ class Champion
          * First of my two overloaded constructors of the Champion class
          */
 	public Champion()   //set the parameters of this constructor
-			{
-			//Give options and take input on champion selection
-                        System.out.println("Pick your champion!");
-                        System.out.println("Press 1 for Braum, press 2 for Morgana");
-                        System.out.println("press 3 for Jinx, or 4 for Zilian");
-                        Scanner usersays = new Scanner(System.in);
-                        int opt = usersays.nextInt();
-                        this.pickHero(opt);
-                        this.arm = new Weapon(this.name);
-			}
+        {
+            //Give options and take input on champion selection
+            System.out.println("Pick your champion!");
+            System.out.println("Press 1 for Braum, press 2 for Morgana");
+            System.out.println("press 3 for Jinx, or 4 for Zilian");
+            Scanner usersays = new Scanner(System.in);
+            int opt = usersays.nextInt();
+            this.pickHero(opt);
+            this.arm = new Weapon();
+        }
         
         /**
          * Second of my overloaded constructors, auto-runs for the npc
          * @param isacomputer 
          */
-	public Champion(boolean isacomputer) {
-		System.out.println("Computer player entering the game");
-
+	public Champion(boolean isacomputer) 
+        {
+            System.out.println("Computer player entering the game");
+            Random randomGenerator = new Random();
+            int randomInt = randomGenerator.nextInt(4);
+            this.pickHero(randomInt);
+            this.arm = new Weapon(true);
 	}
         
         /**
@@ -73,12 +115,15 @@ class Champion
                 case 1: name = "Braum";
                         health = 120;
                         this.attackspeed = 0;
+                        break;
                 case 2: this.name = "Morgana";
                         this.health = 100;
                         attackspeed = 0.1;
+                        break;
                 case 3: this.name = "Jinx";
                         this.health = 90;
                         attackspeed = 0.2;
+                        break;
                 case 4: this.name = "Zilian";
                         this.health = 80;
                         this.attackspeed = 0.1;
@@ -113,12 +158,32 @@ class Weapon {
 
         //REQUIRED: This second class has all private attributes too
 	private int dmg;
+        private double speed;
 	private String name;
 
-	public Weapon(String playername) // this is constructor #1
+	public Weapon() // this is constructor #1
 	{
-		dmg = 20;
+            System.out.println("Choose 1 for saber; 2 for battleaxe.");
+            Scanner usersays = new Scanner(System.in);
+            int opt = usersays.nextInt();
+            if (opt == 1)
+            {
+                dmg = 10; 
+                speed = 0.2;
+                name = "saber";
+            }else{
+                dmg = 20; 
+                speed = 0.1;
+                name = "battleaxe";               
+            }
 	}
+        public Weapon(boolean isacomputer)
+        {
+            dmg = 10; 
+            speed = 0.2;
+            name = "saber";
+            
+        }
 
         /**
          * REQUIRED: Helper asset on the custom class that is used by our
@@ -126,11 +191,15 @@ class Weapon {
          * @return The damage the weapon deals
          */
 	public int getDmg() { 
-		return dmg;
+            return dmg;
 	}
 
 	public String getName() { 
-		return name;
+            return name;
 	}
+        
+        public double getSpeed() {
+            return speed;
+        }
 
 } //closes the weapon class
